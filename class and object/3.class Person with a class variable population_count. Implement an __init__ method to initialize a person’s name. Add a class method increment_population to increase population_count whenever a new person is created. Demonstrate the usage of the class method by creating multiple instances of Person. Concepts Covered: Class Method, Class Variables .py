f9e3461c73a992c97134f5d0ae412e618b4d2d44
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2024-11-27 13:16:10 +0530

    Create 3.class Person with a class variable population_count. Implement an __init__ method to initialize a person’s name. Add a class method increment_population to increase population_count whenever a new person is created. Demonstrate the usage of the class method by creating multiple instances of Person. Concepts Covered: Class Method, Class Variables .py

---
+++ class and object/3.class Person with a class variable population_count. Implement an __init__ method to initialize a person’s name. Add a class method increment_population to increase population_count whenever a new person is created. Demonstrate the usage of the class method by creating multiple instances of Person. Concepts Covered: Class Method, Class Variables .py
@@ -0,0 +1,20 @@
+""" Define a class Person with a class variable population_count. Implement an __init__ method to
+initialize a person’s name. Add a class method increment_population to increase population_count
+whenever a new person is created. Demonstrate the usage of the class method by creating multiple
+instances of Person.
+Concepts Covered: Class Method, Class Variables """
+class Person:
+    population_count = 0
+    def __init__(self,name):
+        self.name = name
+        Person.increament_population()
+    @classmethod
+    def increament_population(cls):
+        cls.population_count +=1
+
+#creating multiple instances of person
+p1 = Person("OM")
+p2 = Person("Ritesh")
+p3 = Person("Rohit")
+
+print(f"Population Count is {Person.population_count}")
