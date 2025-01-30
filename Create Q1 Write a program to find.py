d8d6b137231a5334ace9e64263665acb41fe3868
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2025-01-30 22:26:07 +0530

    Update Create Q1 Write a program to find.py

--- Create Q1 Write a program to find.py
+++ Create Q1 Write a program to find.py
@@ -9,7 +9,7 @@ def longest_common_substring(str1, str2):
     max_len = 0
 
     end_index = 0
-
+    
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if str1[i - 1] == str2[j - 1]:
