3345f598626f25d63e2d66a6ebf1b6400d12739e
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2025-02-05 23:11:05 +0530

    Update LIbrary management system.py

--- LIbrary management system.py
+++ LIbrary management system.py
@@ -2,7 +2,7 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.is_issued = False  # to track if the book is issued or not
+        self.is_issued = False  # to track if the book is issued or not                  
 
     def __str__(self):
         return f"'{self.title}' by {self.author}"
