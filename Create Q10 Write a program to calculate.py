1e386c04a9de1c35b88e5188c2dfe49b1f5ac4b8
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2025-03-05 23:43:57 +0530

    Update Create Q10 Write a program to calculate.py

--- Create Q10 Write a program to calculate.py
+++ Create Q10 Write a program to calculate.py
@@ -6,7 +6,7 @@ def bell_number(n):
 
     bell[0][0] = 1 
 
-    for i in range(1, n + 1):
+    for i in range(1, n + 1):                                                    
         bell[i][0] = bell[i - 1][i - 1]
 
         for j in range(1, i + 1):
